8e7cfa2c861b16f9fa7cc7309f65257cf33c95e5
ali4bulut <66321983+ali4bulut@users.noreply.github.com>
2021-02-04 18:56:06 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ AccelStepper eksen2(1, 4, 5);
 AccelStepper eksen3(1, 6, 7);
 AccelStepper eksen4(1, 8, 9);
 AccelStepper eksen5(1, 10, 11);
+
 AccelStepper eksen6(1, 12, 13);
 
 const int switches[] = {30, 32, 34, 36, 38, 40};
 
-float acilar[6];
+int32_t acilar[6];
 int i = 0;
 
 void Homing(AccelStepper *stepMotor, int homeSwitchNumber)
@@ -49,22 +50,22 @@ void setup()
   Serial.begin(2000000);
   Serial.setTimeout(5);
 
-  eksen1.setMaxSpeed(12800);
+  eksen1.setMaxSpeed(4000);
   eksen1.setAcceleration(9600);
 
-  eksen2.setMaxSpeed(12800);
+  eksen2.setMaxSpeed(4000);
   eksen2.setAcceleration(9600);
 
-  eksen3.setMaxSpeed(12800);
+  eksen3.setMaxSpeed(4000);
   eksen3.setAcceleration(9600);
 
-  eksen4.setMaxSpeed(12800);
+  eksen4.setMaxSpeed(4000);
   eksen4.setAcceleration(9600);
 
-  eksen5.setMaxSpeed(12800);
+  eksen5.setMaxSpeed(4000);
   eksen5.setAcceleration(9600);
 
-  eksen6.setMaxSpeed(12800);
+  eksen6.setMaxSpeed(4000);
   eksen6.setAcceleration(9600);
 
   pinMode(13, OUTPUT);
@@ -89,11 +90,6 @@ void setup()
 
 void loop()
 {
-  // If at the end of travel go to the other end
-  // if (eksen1.distanceToGo() == 0)
-  //   eksen1.moveTo(-eksen1.currentPosition());
-
-  // eksen1.run();
   if (Serial.available() > 0)
   {
     char input[bufferSize + 1];
@@ -104,7 +100,7 @@ void loop()
     while (command != NULL)
     {
       float split = atof(command);
-      acilar[i] = split ;/// 0.05625f; // 1/32 step olduğu için:
+      acilar[i] = split / 1.8f; /// 0.05625f; // 1/32 step olduğu için:
       i++;
 
       command = strtok(NULL, "&");
@@ -117,19 +113,18 @@ void loop()
     }
     Serial.print("\n");
 
-    // eksen1.moveTo(acilar[0]);
-    // eksen1.run();
-    // eksen2.moveTo(acilar[1]);
-    // eksen2.run();
-    // eksen3.moveTo(acilar[2]);
-    // eksen3.run();
-    // eksen4.moveTo(acilar[3]);
-    // eksen4.run();
-    // eksen5.moveTo(acilar[4]);
-    // eksen5.run();
+    eksen1.moveTo(acilar[1]*16);
+    eksen2.moveTo(acilar[2]*64);
+    eksen3.moveTo(acilar[3]*64);
+    eksen4.moveTo(acilar[4]*4);
+    eksen5.moveTo(acilar[5]*4);
     // eksen6.moveTo(acilar[5]);
-    // eksen6.run();
   }
-
+  eksen1.run();
+  eksen2.run();
+  eksen3.run();
+  eksen4.run();
+  eksen5.run();
+  //eksen6.run();
   delay(5);
 }
